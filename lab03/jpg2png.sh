6258d2de4ed781976232e2e0ed13b2b5d0743a37
z5251113 <z5251113@ad.unsw.edu.au>
2020-06-22 23:09:12 +1000

    lab03-q1

--- lab03/jpg2png.sh
+++ lab03/jpg2png.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
+
 SAVEIFS=$IFS
-IFS=$(echo -en "\n\b")
-
-for filename in ` ls *.jpg  `
+IFS=$'\n'
+for filename in $(ls *jpg)
 do
-#  filename_png=$(echo "$filename"|sed s/\.jpg$/ \.png)
-
-#  safename="$(echo "$filename" | sed 's/ /_-_/g')"
-   echo $filename
-#
-   filename_png="${filename%.jpg}.png"
-   if test -e "$filename_png"
-   then
-
+    
+    filename_png=$(echo "$filename"|sed 's/jpg$/png/g')
+#    echo "$filename_png"
+    if test -e "$filename_png"
+    then
         echo "$filename_png already exists"
         exit 1
-   fi
+    fi
 
-   convert "$filename" "${filename%.jpg}.png"
-   rm "$filename"
-
-   echo "$filename"
+    convert "$filename" "$filename_png"
+    rm "$filename"
 done
 IFS=$SAVEIFS
