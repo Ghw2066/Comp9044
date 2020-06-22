2f523270c101d3af815325b59e0a21f889c4183c
z5251113 <z5251113@ad.unsw.edu.au>
2020-06-23 02:13:31 +1000

    lab03-q3

--- lab03/date_image.sh
+++ lab03/date_image.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+SAVEIFS=$IFS
+IFS=$'\n'
+for filename in $(ls *jpg)
+do 
+    
+    datetime=$(ls -l $filename | cut -d" " -f6-8| sed 's/ /_/g')
+    #day=$(ls -l $filename | cut -d" " -f7)
+    #time=$(ls -l $filename | cut -d" " -f8)
+    echo "$datetime"
+    convert -gravity south -pointsize 36 -draw "text 0,10 "$datetime"" "$filename" "tmp_$filename"
+    rm "$filename"
+    mv "tmp_$filename" "$filename"
+    
+done
+IFS=$SAVEIFS
+
+
