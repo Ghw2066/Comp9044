e25eda8769c3d2c10f8f46d237219abe6fbd822e
guohaowen <g50012168@china.huawei.com>
2020-07-13 17:52:11 +0800

    lab05-3.5

--- lab05/snapshot.pl
+++ lab05/snapshot.pl
@@ -1,26 +1,50 @@
 #!/usr/bin/perl -w
+use File::Copy;
 
-$num=0;
+sub save(){
+    $num=0;
+    while ( -e ".snapshot.$num" ) 
+    {
+        print ".snapshot.$num ";
+        
+        $num=$num+1;  
 
-while ( -e ".snapshot.$num" ) 
-{
-    print ".snapshot.$num ";
+        print "$num \n";
+    }
+    mkdir( ".snapshot.$num" ) and print "Creating .snapshot.$num\n";
+
+    $dir = "./*";
+    my @files = glob( $dir );
     
-    $num=$num+1;  
-
-    print "$num \n";
-}
-mkdir( ".snapshot.$num" ) and print "Creating .snapshot.$num\n";
-
-$dir = "./*";
-my @files = glob( $dir );
- 
-foreach (@files ){
-    if (not $_ =~ /snapshot.pl/){
-        `cp "$_" "./.snapshot.$num"`;
-        print $_ . " yes\n";
+    foreach $f (@files ){
+        if (not $f =~ /snapshot.pl/){
+            $dis=".snapshot.$num";
+            copy($f, $dis);
+            #`cp "$f" "./.snapshot.$num"`;
+            print $f . " yes\n";
+        }
     }
 }
+
+sub load($resnum){
+    $resdir=".snapshot.$resnum";
+    my @resfiles = glob( $resdir );
+    foreach $f (@resfiles){
+        copy($f, "../")
+    }
+}
+
+if ($ARGV[0] eq "save"){
+    save();
+}
+
+if ($ARGV[0] eq "load" and $ARGV[1]){ 
+    load($ARGV[1]);
+}
+
+
+
+
 
 
 
