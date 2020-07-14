ced222d3b4e082ee689307226b6986f1573cdc05
Haowen <guohaowen1995@outlook.com>
2020-07-14 20:29:58 +0800

    ass1-init

--- ASST1/shrug-init.sh
+++ ASST1/shrug-init.sh
@@ -1,3 +1,11 @@
 #!/bin/dash
 
-mkdir ".shrug"
+if test -e ".shrug"
+then
+    echo "shrug-init: error: .shrug already exists"
+else
+    mkdir ".shrug"
+    cd .shrug
+    mkdir "index"
+    echo "Initialized empty shrug repository in .shrug"
+fi
