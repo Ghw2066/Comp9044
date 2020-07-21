7415af0e336e53f21c1bdfd1cc7459255e5423d8
Haowen <guohaowen1995@outlook.com>
2020-07-21 22:28:46 +0800

    wktest-7-2

--- weekly_exam/middle_lines.pl
+++ weekly_exam/middle_lines.pl
@@ -7,7 +7,7 @@ close(DATA);
 if(@lines%2==0){
     #$n=@lines/2;
     printf "$lines[@lines/2-1]";
-    printf "$lines[@lines]";
+    printf "$lines[@lines/2]";
 }
 else{
     printf "$lines[(@lines-1)/2]";
