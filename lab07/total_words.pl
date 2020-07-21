a31d4d96ce46bbee0c663715770313022dda904c
guohaowen <g50012168@china.huawei.com>
2020-07-21 16:44:48 +0800

    lab07-1

--- lab07/total_words.pl
+++ lab07/total_words.pl
@@ -1,2 +1,35 @@
 #!/usr/bin/perl -w
 
+$n=0;
+while(defined($line = <STDIN>)){
+    @words = split(/[a-zA-Z]+/, $line);
+    foreach $_ (@words) {
+        #if ($number ne '') {
+            #$total += $number;
+        
+        $n++;
+        #}
+        # unless($word =~ /[a-zA-Z]+/){
+        #     printf "$word ";
+        # }
+
+    }
+    $m++;
+    #printf "\n";
+}
+
+#@input_text_array = <>;
+#$input_text_array = <STDIN>;
+#join "", @input_text_array;
+
+#@numbers = split(/[a-zA-Z]+/, $input_text_array);
+#print join(",", @numbers), "\n";
+
+# foreach $number (@numbers) {
+#     #if ($number ne '') {
+#         $total += $number;
+#         $n++;
+#     #}
+# }
+$n=$n-$m;
+printf "$n words\n";
