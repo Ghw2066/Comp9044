731f016d867ec3165c2a525ffbe76a9f55cf97ca
Haowen <guohaowen1995@outlook.com>
2020-07-22 22:51:26 +0800

    wktest07-3

--- weekly_exam/largest_numbered_line.pl
+++ weekly_exam/largest_numbered_line.pl
@@ -7,24 +7,40 @@
 # $userinput =  <STDIN>;
 # chomp ($userinput);
 # print "User typed $userinput\n";
-
-while (<STDIN>) {
+# while ($line = <>) {
+#     if ($line =~ /^\D*(-?\d+(\.\d+)?)/) {
+#         print "$1\n";
+#     }
+# }
+while ($line = <>) {
     #last unless /\S/;
-    print "$_";
-    push @lines, $_;
+    print "$line";
+    push @lines, $line;
     
 }
-print @lines;
+#print @lines;
 
-# foreach $line (@lines){
-#     @words=split(/\s+/,$line);
-#     $max=0;
-#     foreach $word (@words){
-#         if($word =~ /[0-9.]+/ and $&>$max){
-#             $num = $&;
-#         }
-#     }  
-#     push @nums, $num;
-# }
+foreach $line (@lines){
+    @words=split(/\s+/,$line);
+    $max=0;
+    foreach $word (@words){
+        if($word =~ /-?\d+(\.\d+)?/ and $&>$max){
+            $max = $&;
+            # print "$&";
+        }
+    }  
+    push @nums, $max;
+}
+$max=0;
+foreach $num (@nums){
+    if($num>$max){
+        $max=$num;
+    }
+}
 
-# print "@nums\n";
+for ($i=0; $i<@lines; $i=$i+1){
+    if($max==$nums[$i]){
+        print "$lines[$i]\n";
+    }
+}
+#print "@nums\n";
